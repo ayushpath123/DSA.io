57c4cc0dfa8a5f78617ff2179808a7da41b7d3aa
Ayush Pathak <122785367+ayushpath123@users.noreply.github.com>
2024-10-21 22:12:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,18 @@ import Premium from "./Components/Premium.jsx"
 import Community from "./Components/Community.jsx"
 import Footer from "./Components/Footer.jsx"
  function App() {
+    useEffect(() => {
+    // Dynamically add the widget script after the component mounts
+    const script = document.createElement("script");
+    script.src = "https://nexx-widget.vercel.app/widget.umd.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div className=" flex flex-col gap-10  bg-white-100">
       <Header></Header>
@@ -17,7 +29,6 @@ import Footer from "./Components/Footer.jsx"
       <Community></Community>
       <Footer></Footer>
        <my-widget project-id="316"></my-widget>
-      <script src="https://nexx-widget.vercel.app/widget.umd.js"></script>
     </div>
   )
 }
